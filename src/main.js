16398d16d8e03e0ce62f57d445401d4008661834
vckin <969766092@qq.com>
2019-04-28 20:58:31 +0800

    路由 登录 跳转

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router'
+import './directives'
+import './components'
+import store from './store'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App/>'
 })
